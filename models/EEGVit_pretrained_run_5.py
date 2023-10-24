5c4b9b2aa3760849cc30dea21fe0d31c10440717
NengZhou <Vizards8@outlook.com>
2023-10-23 20:47:24 -0400

    feat: adjust parameters

---
+++ models/EEGVit_pretrained_run_5.py
@@ -0,0 +1,40 @@
+import math
+import numpy as np
+import torch
+from torch import nn
+from torch.utils.data import Dataset, DataLoader, Subset
+import transformers
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+class EEGViT_pretrained(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=256,
+            kernel_size=(1,16),
+            stride=(1,16),
+            padding=(0,6),
+            bias=False
+        )
+        self.batchnorm1 = nn.BatchNorm2d(256, False)
+        model_name = "google/vit-base-patch16-224"
+        config = transformers.ViTConfig.from_pretrained(model_name)
+        config.update({'num_channels': 256})
+        config.update({'image_size': (129,32)})
+        config.update({'patch_size': (129,1)})
+
+        model = transformers.ViTForImageClassification.from_pretrained(model_name, config=config, ignore_mismatched_sizes=True)
+        model.vit.embeddings.patch_embeddings.projection = torch.nn.Conv2d(256, 768, kernel_size=(129,1), stride=(129,1), padding=(0,0), groups=256)
+        model.classifier=torch.nn.Sequential(torch.nn.Linear(768,1000,bias=True),
+                                     torch.nn.Dropout(p=0.1),
+                                     torch.nn.Linear(1000,2,bias=True))
+        self.ViT = model
+
+    def forward(self,x):
+        x=self.conv1(x)
+        x=self.batchnorm1(x)
+        x=self.ViT.forward(x).logits
+
+        return x
