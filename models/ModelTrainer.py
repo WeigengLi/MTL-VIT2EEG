77357b312508ca656c530043665f96d3b01759f3
WeigengLi <liweigeng2014@hotmail.com>
2023-11-21 00:46:00 -0500

    feat: updating MTL_ADDA_Trainer4

--- models/ModelTrainer.py
+++ models/ModelTrainer.py
@@ -544,6 +544,103 @@ class MTL_ADDA_Trainer3(ModelTrainer):
             return loss
 
 
+class MTL_ADDA_Trainer4(ModelTrainer):
+    def __init__(self, model, Dataset, optimizer, discriminator, scheduler=None, batch_size=64, n_epoch=15, weight = 1, Trainer_name='Trainer') -> None:
+        self.discriminator = discriminator
+        self.weight = weight
+        super().__init__(model, Dataset, optimizer,
+                         scheduler, batch_size, n_epoch, Trainer_name)
+        self.STL_Trainer = STL_Trainer(model, Dataset, optimizer,
+                         scheduler, batch_size, n_epoch, Trainer_name)
+
+
+    def initialization(self):
+        super().initialization()
+        self.discriminator_optimizer =  torch.optim.Adam(self.discriminator.parameters(), lr=1e-4)
+        self.discriminator_scheduler = torch.optim.lr_scheduler.StepLR(self.discriminator_optimizer, step_size=6, gamma=0.1)
+        self.discriminator.to(self.device)
+        self.BCE_criterion = nn.BCEWithLogitsLoss()
+        self.BCE_criterion.to(self.device)
+        # 计算复制 target_loader 的倍数，以使其长度不小于 source_loader
+
+
+    
+    def model_evaluate(self, stage, data_loader, epoch):
+
+        if stage != TRAIN_STAGE:
+            device = self.device
+            optimizer = self.optimizer
+            
+            BCE_criterion = self.BCE_criterion
+            epoch_loss = 0.0
+            epoch_position_loss = 0.0
+            epoch_domain_loss = 0.0
+            source_loader = self.train_loader
+            target_loader = self.test_loader
+            c_target_loader = cycle(target_loader)
+            batches = zip(source_loader,  cycle(target_loader))
+            first_batch = next(batches)
+            n_batches = len(source_loader)
+            for i, ((source_x, source_labels, index), (target_x, trage_y, index2) )in tqdm(enumerate(batches), total=n_batches):
+                optimizer.zero_grad()
+                self.discriminator_optimizer.zero_grad()
+                x = torch.cat([source_x, target_x])
+                x = x.to(device)
+                domain_y = torch.cat([torch.ones(source_x.shape[0]),
+                                    torch.zeros(target_x.shape[0])])
+                domain_y = domain_y.to(device)
+                label_y = source_labels.to(device)
+
+                positions, shared_features = self.model(x)
+                domain_preds = self.discriminator(shared_features).squeeze()
+                label_preds = positions[:source_x.shape[0]]
+
+                domain_loss = BCE_criterion(domain_preds, domain_y)
+                position_loss = MSE_criterion(label_preds, label_y)
+
+                domain_loss.backward(retain_graph=True)
+                loss = position_loss-domain_loss.item()*self.weight
+                loss.backward()
+                self.discriminator_optimizer.step()
+                optimizer.step()
+                
+                epoch_loss += loss.item()
+                epoch_position_loss += position_loss.item()
+                epoch_domain_loss += domain_loss.item()
+                 # Print the loss and accuracy for the current batch
+                if stage == TRAIN_STAGE and i % 100 == 0:
+                    print(f"Epoch {epoch}, Batch {i}, position loss: {position_loss.item()} " +
+                        f" RMSE(mm): {default_round(Cal_RMSE(position_loss.item()))} " +
+                        f" domain loss: {domain_loss.item()}")
+                    
+            loss = {'overall_loss': epoch_loss / len(data_loader),
+                    'position_loss': epoch_position_loss / len(data_loader),
+                    'position_RMSE': Cal_RMSE(epoch_position_loss / len(data_loader)),
+                    'domain_loss': domain_loss / len(data_loader)
+                    }
+
+            return loss
+            
+        # Test and Val stage is the same as Single Task Learning
+        else:
+            return self.STL_Trainer.model_evaluate(stage, data_loader, epoch)
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # region helper functions
 
 def split(ids, train, val, test):
