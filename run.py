b5d8c19b7cebdfa9339ee33b0ae1c76e3271ecd0
NengZhou <Vizards8@outlook.com>
2023-11-22 19:16:06 -0500

    feat: add ViT_reconstruct_v8

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ from dataset.Datasets import EEGEyeNetDataset, MTLPupilDataset
 # TODO: ADD COMMIT about possible models and instructions
 from models.STL import EEGViT_pretrained, InceptionViT_pretrained,EEGViT_pretrained_hierachical
 from models.MTL_pretrained import ViT_reconstruct
-from models.Vit_reconstruct_1116 import ViT_reconstruct_modified
+from models.ViT_reconstruct_v8 import ViT_reconstruct_v8
 from models.ModelTrainer import STL_Trainer, MTL_RE_Trainer, MTL_PU_Trainer
 
 
@@ -40,7 +40,7 @@ TASKS_TRAINER = {
 
 # region Task Config
 DEFAULT_TASK = MTL_RE_STR
-DEFAULT_MODEL = ViT_reconstruct_modified
+DEFAULT_MODEL = ViT_reconstruct_v8
 NEW_DATA_PATH = False
 NUM_ITER = 5
 # endregion
@@ -52,7 +52,7 @@ def main():
         model = DEFAULT_MODEL()
         optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
         scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.1)
-        mt = TASKS_TRAINER[DEFAULT_TASK](model, Dataset, optimizer, scheduler, batch_size=64, n_epoch=15, weight=100,Trainer_name=f'ViT_reconstruct_modified{str(i+1)}')
+        mt = TASKS_TRAINER[DEFAULT_TASK](model, Dataset, optimizer, scheduler, batch_size=64, n_epoch=15, weight=100,Trainer_name=f'ViT_reconstruct_v8{str(i+1)}')
         mt.run()
 
 if __name__ == '__main__':
